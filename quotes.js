3e2bca7ab53743a89121455d988ae7425db61888
DavidCastefa <80602906+DavidCastefa@users.noreply.github.com>
2021-04-03 19:43:40 +0200

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,52 @@
+
+const QUOTEBANK = [
+  {
+    quote: "Life is what happens when you’re busy making other plans.",
+    author: "John Lennon"
+  },
+  {
+    quote: "Money and success don’t change people;\nthey merely amplify what is already there.",
+    author: "Will Smith"
+  },
+  {
+    quote: "You never really learn much from hearing yourself speak.",
+    author: "George Clooney"
+  },
+  {
+    quote: "Watch your thoughts; they become words.\nWatch your words; they become actions.\nWatch your actions; they become habits.\nWatch your habits; they become character.\nWatch your character; it becomes your destiny.",
+    author: "Lao-Tze"
+  },
+  {
+    quote: "Do not be anxious about tomorrow, for tomorrow will be anxious for itself.\nLet the day's own trouble be sufficient for the day.",
+    author: "Jesus Christ"
+  },
+  {
+    quote: "For what shall it profit a man, if he gain the whole world, and suffer the loss of his soul?",
+    author: "Jesus Christ"
+  },
+  {
+    quote: "If you want your children to listen, try talking softly to someone else.",
+    author: "Ann Landers"
+  },
+  {
+    quote: "A day without laughter is a day wasted.",
+    author: "Charlie Chaplin"
+  },
+  {
+    quote: "If you’re too open-minded, your brains will fall out.",
+    author: "Lawrence Ferlinghetti"
+  },
+  {
+    quote: "You either walk inside your story and own it or you stand outside your story and hustle for your worthiness.",
+    author: "Brené Brown"
+  },
+  {
+    quote: "Let go of who you think you’re supposed to be;\nembrace who you are.",
+    author: "Brené Brown"
+  },
+  {
+    quote: "I intend to live forever, or die trying.",
+    author: "Groucho Marx"
+  }
+];
+//export { QUOTEBANK };
